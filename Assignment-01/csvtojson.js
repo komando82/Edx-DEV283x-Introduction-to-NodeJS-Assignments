abd9bbe6d0ea7d178cac2ab3ec3ea266f9addb7c
Ivan <neradovicivan@yahoo.com>
2017-11-21 20:48:09 +0100

    adding error for fs.trunckate

--- Assignment-01/csvtojson.js
+++ Assignment-01/csvtojson.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
+const csv = require('csvtojson')
 
 const csvFilePath='customer-data.csv'
-const csv=require('csvtojson')
 
 csv()
 	.fromFile(csvFilePath)
 	.on("end_parsed",function(jsonObj){ //when parse finished, result will be emitted here.
      	//console.log(jsonObj)
-     	fs.truncate('customer-data.json', 0, function() {
+     	fs.truncate('customer-data.json', 0, function(error) {
+     		if (error) return console.error(error)
+     			
 	     	fs.writeFile('customer-data.json', JSON.stringify(jsonObj, null, 2), function (error) {
 			  	if (error) return console.error(error)
 			  	//console.log('Writing is done.')
