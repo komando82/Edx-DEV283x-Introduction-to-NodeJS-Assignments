dfe22a91e013e645620b3cda6dd8963f6a52de1e
Ivan <neradovicivan@yahoo.com>
2017-11-21 20:50:18 +0100

    fixing identation

--- Assignment-01/csvtojson.js
+++ Assignment-01/csvtojson.js
@@ -6,16 +6,16 @@ const csvFilePath='customer-data.csv'
 csv()
 	.fromFile(csvFilePath)
 	.on("end_parsed",function(jsonObj){ //when parse finished, result will be emitted here.
-     	//console.log(jsonObj)
-     	fs.truncate('customer-data.json', 0, function(error) {
-     		if (error) return console.error(error)
-     			
-	     	fs.writeFile('customer-data.json', JSON.stringify(jsonObj, null, 2), function (error) {
-			  	if (error) return console.error(error)
-			  	//console.log('Writing is done.')
+		//console.log(jsonObj)
+		fs.truncate('customer-data.json', 0, function(error) {
+			if (error) return console.error(error)
+
+			fs.writeFile('customer-data.json', JSON.stringify(jsonObj, null, 2), function (error) {
+				if (error) return console.error(error)
+				//console.log('Writing is done.')
 			})
 		})
-   	})
+	})
 	.on('done',(error)=>{
-	    //console.log('end')
+		//console.log('end')
 	})
